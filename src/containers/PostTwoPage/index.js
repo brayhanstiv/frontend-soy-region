b2d92ea90c0a87793da90a3e91ce8740abc5fe56
jhoan sebastian <jhoansebax257@gmail.com>
2022-01-19 20:14:27 -0500

    feat: news

--- src/containers/PostTwoPage/index.js
+++ src/containers/PostTwoPage/index.js
@@ -2,7 +2,7 @@ import React, {Fragment, useState} from 'react';
 import BreadCrumb from "../../components/BreadCrumb";
 import FontAwesome from "../../components/uiStyle/FontAwesome";
 import {Link} from "react-router-dom";
-import WidgetTab from "../../components/WidgetTab";
+// import WidgetTab from "../../components/WidgetTab";
 import BannerSection from "../../components/BannerSection";
 import PostOnePagination from "../../components/PostOnePagination";
 import OurBlogSection from "../../components/OurBlogSection";
